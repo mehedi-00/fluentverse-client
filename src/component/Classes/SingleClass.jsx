bca9f26f4abb58b171eaf7e60ee5d1f6f7adba0f
Mehedi <developermehedi128@gmail.com>
2023-06-10 16:08:27 +0600

    select class added

--- src/component/Classes/SingleClass.jsx
+++ src/component/Classes/SingleClass.jsx
@@ -1,6 +1,16 @@
+import {  useLocation, useNavigate } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { toast } from "react-hot-toast";
 
 const SingleClass = ({singleClass}) => {
+    const [axiosSecure]  = useAxiosSecure()
+    const {user} = useAuth()
+    const location = useLocation();
+    const navigate = useNavigate()
     const { 
+        _id,
         image,
         instructor_name,
         instructor_email,
@@ -8,6 +18,44 @@ const SingleClass = ({singleClass}) => {
         avilable_seats,
         class_name
         } = singleClass
+
+        const handleSelect= ()=>{
+            
+               if(user && user?.email){ 
+                const newData = {
+                    class_id: _id,
+                    email: user?.email,
+                    class_image: image,
+                    class_name: class_name,
+                    price: price
+                }
+            
+                
+                axiosSecure.post('/select-class', {newData})
+                .then(data=>{
+                    if(data.data.insertedId){
+                        toast.success('Successfully seleced class!')
+                    }
+                })
+            }  else{
+                    Swal.fire({
+                        title: 'Please Login First?',
+                        showCancelButton: true,
+                        confirmButtonText: 'Login',
+                      }).then((result) => {
+                        /* Read more about isConfirmed, isDenied below */
+                        if (result.isConfirmed) {
+                            navigate('/login' , {state: {from:location}}) 
+                        } 
+                      })
+                }
+            
+
+        }
+
+
+
+
     return (
         <div className="relative flex  flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md  shadow-black">
   <div className="relative mx-4 mt-4 h-64 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
@@ -40,6 +88,7 @@ const SingleClass = ({singleClass}) => {
   </div>
   <div className="p-6 pt-0 mx-auto">
     <button
+    onClick={handleSelect}
       className=" btn btn-primary"
     >
       Select
